389b530cb1036313dca4054d4cf584b92ae7a3af
kBohdan <bkachmar1@gmail.com>
2021-06-27 23:24:56 +0300

    menu

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const btnOpenMenu = document.querySelector("#btn-menu");
+const closedBtnMenu = document.querySelector("#closed-btn-menu");
+const menu = document.querySelector(".menu");
+
+btnOpenMenu.onclick = function() {
+    menu.style.top = "0%";
+}
+
+closedBtnMenu.onclick = function() {
+    menu.style.top = "-100%";
+}
